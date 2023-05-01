aaf1b26844ebee6b95da0b0ceb6c08f824188437
Phong Cao <phongcao0714@gmail.com>
2023-05-01 11:42:17 -0700

    Create project skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section intro">
+        <h1>Blizzard: Workers Rights</h1>
+      </div>
+      <div className="section info part1">
+        <div className="header">
+          <h2>Part I:<br/>The Workers Conditions</h2>
+        </div>
+
+        
+        <div className="text">
+          {/* <h4>
+            Long-standing issues regarding labor rights plagues the workers in the video game industry.
+          </h4>
+          <ul>
+            <li>Long Working Hours</li>
+            <li>Low Pays</li>
+            <li>Job Security</li>
+            <li>Racism</li>
+            <li>Sexual Harrassment</li>
+            <li>...</li>
+          </ul> */}
+
+        </div>
+        
+      </div>
+
+      <div className="section info part2">
+        <div className="header">
+          <h2>Part II:<br/>Collectivization</h2>
+        </div>
+
+        
+        <div className="text">
+
+
+        </div>
+        
+      </div>
+
+      <div className="section info part3">
+        <div className="header">
+          <h2>Part III:<br/>Blizzard Strikes Back</h2>
+        </div>
+
+        
+        <div className="text">
+
+
+        </div>
+        
+      </div>
+
+      <div className="section info part4">
+        <div className="header">
+          <h2>Part IV:<br/>The Struggle Continues</h2>
+        </div>
+
+        
+        <div className="text">
+
+
+        </div>
+        
+      </div>
+
     </div>
   );
 }
