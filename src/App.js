299b303841bfe307cc519c26c6b206ca499ea114
Phong Cao <phongcao0714@gmail.com>
2023-05-01 19:51:13 -0700

    Add basic info

--- src/App.js
+++ src/App.js
@@ -13,17 +13,16 @@ function App() {
 
         
         <div className="text">
-          {/* <h4>
-            Long-standing issues regarding labor rights plagues the workers in the video game industry.
-          </h4>
+          <h3>
+            Long-standing issues regarding labor rights plague workers in the video game industry.
+          </h3>
           <ul>
             <li>Long Working Hours</li>
             <li>Low Pays</li>
             <li>Job Security</li>
             <li>Racism</li>
             <li>Sexual Harrassment</li>
-            <li>...</li>
-          </ul> */}
+          </ul>
 
         </div>
         
@@ -36,7 +35,13 @@ function App() {
 
         
         <div className="text">
-
+          <h3>
+            Two seperate groups of QA testers at Blizzard had collectivized and formed union within their respective teams.
+          </h3>
+          <ul>
+            <li>Ravens Software</li>
+            <li>Blizzard Albany</li>
+          </ul>
 
         </div>
         
@@ -47,12 +52,19 @@ function App() {
           <h2>Part III:<br/>Blizzard Strikes Back</h2>
         </div>
 
-        
         <div className="text">
+          <h3>
+            In response to the collectivization efforts by its workers, Blizzard employed illegal and unethicals tactics and strategies to disrupt their efforts.
+          </h3>
+          <ul>
+            <li>Witheld wage increases to Union Workers</li>
+            <li>Demanded expansion of Union voting pool to dilute workers voting power</li>
+            <li>Hired Union busting law firm</li>
+            <li>Purposefully delaying union negotiations</li>
+          </ul>
 
-
-        </div>
-        
+        </div> 
+       
       </div>
 
       <div className="section info part4">
@@ -62,9 +74,15 @@ function App() {
 
         
         <div className="text">
+          <h3>
+            The workers at Blizzard continued with their struggles for labor rights ahead of the resistances from Blizzard.
+          </h3>
+          <ul>
+            <li>NLRB found Blizzard's actions to be in violation of Workers Rights</li>
+            <li>The successful formation of unions at Blizzard sparks hopes among workers of the Video Game Industry</li>
+          </ul>
 
-
-        </div>
+        </div> 
         
       </div>
 
